40360dad65f8111dd2cb7fb5747adb3598531fee
kristel <kristel1menegue89@gmail.com>
2023-03-20 11:53:10 -0600

    se agrego un nombre

---
+++ Laboratorio 2/laboratorio.py
@@ -0,0 +1,18 @@
+edad = 17 
+print (edad)
+
+
+nombre= "kristel" 
+print (nombre)
+
+pi= 3.1416
+print (pi)
+
+van_a_ganar_el_anno = True
+print (van_a_ganar_el_anno)
+
+print (type(edad))
+print (type(nombre))
+print (type(pi))
+print (type(van_a_ganar_el_anno))
+
