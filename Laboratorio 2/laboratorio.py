8f6226a5c1b4b84242688cdf4ef2609f3129975d
kristel <kristel1menegue89@gmail.com>
2023-03-22 08:37:19 -0600

    se agrego linea con texto aleatorio

--- Laboratorio 2/laboratorio.py
+++ Laboratorio 2/laboratorio.py
@@ -1,5 +1,7 @@
 edad = 17 
 print (edad)
+''' Esta variable indica mi edad
+Me falta poco para ser mayor '''
 
 
 nombre= "kristel" 
@@ -7,6 +9,7 @@ print (nombre)
 
 pi= 3.1416
 print (pi)
+#Esta variable indica el valor de pi 
 
 van_a_ganar_el_anno = True
 print (van_a_ganar_el_anno)
@@ -15,4 +18,23 @@ print (type(edad))
 print (type(nombre))
 print (type(pi))
 print (type(van_a_ganar_el_anno))
+
+DIAS_DE_LA_SEMANA = 7
+print (DIAS_DE_LA_SEMANA)
+
+print (f"mi nombre es {nombre} y me gustan los chetos")
+
+num1= 4
+num2= 4
+resultado= num1 + num2 
+print (resultado)
+
+resultado2= num1 - resultado
+print(resultado2)
+
+resultado3= resultado2 * num2 
+print (resultado3)
+
+numero= 50 % 5
+print (numero)
 
